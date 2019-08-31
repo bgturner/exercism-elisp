d64d85650c14a925398ea8d94a92a226e9bae76f
Benjamin Turner <benjamin@passionsplay.com>
2019-08-30 21:36:39 -0700

    Add hello-world code

---
+++ hello-world/hello-world.el
@@ -0,0 +1,13 @@
+;;; hello-world.el --- Hello World Exercise (exercism)
+
+;;; Commentary:
+
+;;; Code:
+
+(defun hello ()
+  "Exercism exercise"
+  (interactive)
+  (print "Hello, World!"))
+
+(provide 'hello-world)
+;;; hello-world.el ends here
